7832ebe542fa8205126a5c87b6c85d50aef4c9da
firuz77777 <namefiruzrahmiddinov33@gmail.com>
2022-11-07 16:45:01 +0500

    uch

--- While03.py
+++ While03.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    c = 0
+    i = 0
+    while i < len(s):
+        if not s[i].isdigit() and not s[i].isalpha():
+            c = c + 1
+        i = i + 1
+    return c 
+print(main('dicj;;s'))
